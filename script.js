9cdc78b8b8e94dff850cc9597846ae23f05f9caa
SirajAtFiverr <sirajatfiverr@gmail.com>
2023-06-26 09:00:15 +0500

    save changes

--- script.js
+++ script.js
@@ -14,6 +14,8 @@ let symbolsAdded = false;
 let lengthOfPasswordEl = document.getElementsByClassName("password-length")[0];
 let addNumbersEl = document.getElementsByClassName("add-numbers")[0];
 let addSymbolsEl = document.getElementsByClassName("add-symbols")[0];
+let password1El = document.getElementsByClassName("password-1")[0];
+let password2El = document.getElementsByClassName("password-2")[0];
 
 addNumbersEl.addEventListener("click", function(){
     if(numbersAdded){
@@ -39,37 +41,42 @@ addSymbolsEl.addEventListener("click", function(){
     }
 });
 
-function getRandomCharacter(){
-    return Mta
+function getRandomCharacter(characters){
+    return characters[Math.floor(Math.random() * characters.length)]
 }
 
 function generatePassword(){
 
     let characters = alphabets;
     if(numbersAdded){
-        characters = alphabets.concat(numbers);
+        characters = characters.concat(numbers);
     }
     if(symbolsAdded){
-        characters = alphabets.concat(symbols);
+        characters = characters.concat(symbols);
     }
 
     let userInput = lengthOfPasswordEl.value;
     if(userInput){
+
         lengthOfPassword = userInput;
+        if(userInput > maxLength){
+            lengthOfPassword = 20;
+        }else if(userInput < minLength){
+            console.log(userInput);
+            lengthOfPassword = 5;
+        }
+
     }else{
         lengthOfPassword = 15;
-    }
-
-    if(userInput > maxLength){
-        lengthOfPassword = 20;
-    }else if(userInput < minLength){
-        lengthOfPassword = 5;
     }
 
     let password1 = '';
     let password2 = '';
     for(let i = 0; i < lengthOfPassword; i++){
-
+        password1 += getRandomCharacter(characters);
+        password2 += getRandomCharacter(characters);
     }
-    console.log(userInput, lengthOfPassword);
+    
+    password1El.textContent = password1;
+    password2El.textContent = password2;
 }
